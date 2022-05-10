05324e7a5aa06753e418a7672fddaee5752b8548
Derek Vogt <derekvogt3@gmail.com>
2022-05-10 14:44:33 -0500

    commit tree c37de268b656b0d8cb415e15ffbd3ad90de471b4

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+var employee = {
+  name: "Dan",
+  streetAddress: "Test address",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
